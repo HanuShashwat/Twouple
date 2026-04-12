29e0b630a1c51f8b8ab2ddd1199e381005e95647
Hanu Shashwat <hanushashwat733@gmail.com>
2026-04-12 22:47:43 +0530

    feat(app.dart): create main application widget with multi bloc provider and material app router

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/auth/data/auth_repository.dart';
+
+class TwoupleApp extends StatelessWidget {
+  const TwoupleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (context) => AuthBloc(AuthRepository()),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Twouple',
+        theme: AppTheme.darkTheme,
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
